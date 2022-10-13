d4275b4681d53a63935969ecedb7a81f5f163b11
7amim <tufaruk7@gmail.com>
2022-10-13 14:07:15 -0400

    Update movingAverageAlgorithm.cpp

--- movingAverageAlgorithm.cpp
+++ movingAverageAlgorithm.cpp
@@ -81,61 +81,61 @@ int main() {
 	was the value 10 of ten lines. Found via trial and error.
 	Also resets the array values during this period */
 
-    while (sensorValues.good()) {
-    	if (flag == true) {
-    		for (int i = 0; i < 10; i++) {
-    			yArray[i] = 0;
-			  	zArray[i] = 0;
-    			sensorValues >> a >> b >> time>> c >> accelY >> accelZ >> c >> gyroY >> ignoreLine;
-    			if (gyroY >= 0 && gyroY <= 1000) {
-    				footflat << lines << " " << gyroY << endl;
-				}
-    			plotValues << gyroY << " " << accelY << " " << accelZ << endl;
-    			lines++;
-			}
-			flag = false;
-		} else { // flag =/= true, need to find next heel strike
-	    	sensorValues >> a >> b >> time>> c >> accelY >> accelZ >> c >> gyroY >> ignoreLine;
-	    	zArray[lines%10] = accelZ;
-	    	yArray[lines%10] = accelY;
-			double zAvg = 0;
-			double yAvg = 0;
-			/* Considered a weighted average.
-			The thresholds were concluded through
-			eye-balled data analysis and trial/error*/
-			for (int i = 0; i < 10; i++) {
-				if (zArray[i] >= 20000)
-					zArray[i] *= 1.25;
-	    		zAvg += zArray[i];
-	    		if (yArray[i] <= -9000)
-	    			yArray[i] *= 1.5;
-	    		yAvg += yArray[i];
-			}
-			if (yAvg/10 < Y_THRESHHOLD && zAvg/10 > Z_THRESHHOLD)  {
-				cout << "Y_AVG IS " << yAvg/10 << " Heel Strike at " << time  << " line " << lines << endl;
-				numSteps++;
-				heelStrikes << lines << " " << yAvg/10 << " " << zAvg/10 << endl;
-				flag = true;
-				flag2 = true;
-			}
-			else {
-				//Flag2 is used to buffer when the next toe up phase will be
-				if (zAvg/10 > Z_THRESHHOLD && flag2==true) {
-					toeUp << lines << " " << zAvg/10 << endl;
-					flag2=false;
-				} else if (accelZ >= 0 && gyroY >= -1000 && gyroY <=1000) {
-					midStance << lines << " " << gyroY << endl;
-					footflat << lines << " " << gyroY << endl;
-				} else if (gyroY >= -1000 && gyroY <=1000) {
-    				footflat << lines << " " << gyroY << endl;
-				} else if (gyroY <= -8000) {
-					midSwing << lines << " " << gyroY << endl;
-				}
-				plotValues << gyroY << " " << accelY << " " << accelZ << endl;
-			}
-			lines++;
-		}
+  while (sensorValues.good()) {
+    if (flag == true) {
+      for (int i = 0; i < 10; i++) {
+        yArray[i] = 0;
+        zArray[i] = 0;
+        sensorValues >> a >> b >> time>> c >> accelY >> accelZ >> c >> gyroY >> ignoreLine;
+        if (gyroY >= 0 && gyroY <= 1000) {
+          footflat << lines << " " << gyroY << endl;
+      }
+        plotValues << gyroY << " " << accelY << " " << accelZ << endl;
+        lines++;
     }
+    flag = false;
+    } else { // flag =/= true, need to find next heel strike
+      sensorValues >> a >> b >> time>> c >> accelY >> accelZ >> c >> gyroY >> ignoreLine;
+      zArray[lines%10] = accelZ;
+      yArray[lines%10] = accelY;
+      double zAvg = 0;
+      double yAvg = 0;
+      /* Considered a weighted average.
+      The thresholds were concluded through
+      eye-balled data analysis and trial/error*/
+      for (int i = 0; i < 10; i++) {
+        if (zArray[i] >= 20000)
+          zArray[i] *= 1.25;
+          zAvg += zArray[i];
+          if (yArray[i] <= -9000)
+            yArray[i] *= 1.5;
+          yAvg += yArray[i];
+      }
+      if (yAvg/10 < Y_THRESHHOLD && zAvg/10 > Z_THRESHHOLD)  {
+        cout << "Y_AVG IS " << yAvg/10 << " Heel Strike at " << time  << " line " << lines << endl;
+        numSteps++;
+        heelStrikes << lines << " " << yAvg/10 << " " << zAvg/10 << endl;
+        flag = true;
+        flag2 = true;
+      }
+    else {
+      //Flag2 is used to buffer when the next toe up phase will be
+      if (zAvg/10 > Z_THRESHHOLD && flag2==true) {
+        toeUp << lines << " " << zAvg/10 << endl;
+        flag2=false;
+      } else if (accelZ >= 0 && gyroY >= -1000 && gyroY <=1000) {
+        midStance << lines << " " << gyroY << endl;
+        footflat << lines << " " << gyroY << endl;
+      } else if (gyroY >= -1000 && gyroY <=1000) {
+          footflat << lines << " " << gyroY << endl;
+      } else if (gyroY <= -8000) {
+        midSwing << lines << " " << gyroY << endl;
+      }
+      plotValues << gyroY << " " << accelY << " " << accelZ << endl;
+    }
+    lines++;
+  }
+  }
   cout << "Number of lines read is: " << lines << endl;
   cout << "Number of steps is: " << numSteps;
 }
